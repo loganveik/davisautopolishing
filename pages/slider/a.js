97ae328865debe1e4cdbd030d67f29568c3ecdde
ljv29 <jumpswervelogan@gmail.com>
2022-08-28 05:08:18 -0700

    final draft site done, just need to add modals and finish packages carosel

--- pages/slider/a.js
+++ pages/slider/a.js
@@ -1,8 +1,7 @@
-// SWIPER
-const swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
-    // spaceBetween: 30,
-    // slidesPerGroup: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
     loop: true,
     loopFillGroupWithBlank: true,
     pagination: {
