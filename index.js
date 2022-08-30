c77466176005942173be513ae608288fa853ff40
ljv29 <jumpswervelogan@gmail.com>
2022-08-30 01:36:47 -0700

    added gallery pic plus used SwiperJS library for the image gallery modal popup

--- index.js
+++ index.js
@@ -35,3 +35,41 @@ faqListItems.forEach(faqListItem => {
         faqListItem.classList.toggle("active");
     });
 });
+
+// JS for SwiperJS CDN for gallery image slider and package card carousel
+const swiper = new Swiper(".swiper", {
+    cssMode: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+});
+
+const viewBtns = document.querySelectorAll(".view-more-btn");
+viewBtns.forEach(viewBtn => {
+    viewBtn.addEventListener("click", () => {
+        navMobile.classList.toggle("active");
+    });
+});
+
+const closeModalBtn = document.querySelector(".overlay-close-icon");
+const openModalBtn = document.querySelector(".overlay-open-icon");
+const modalOverlay = document.querySelector(".modal-overlay");
+const body = document.querySelector("body");
+
+openModalBtn.addEventListener("click", () => {
+    modalOverlay.classList.add("active");
+    // body.css("overflow", "hidden");
+    body.style.overflow = "hidden";
+});
+
+closeModalBtn.addEventListener("click", () => {
+    modalOverlay.classList.remove("active");
+    // body.css("overflow", "auto");
+    body.style.overflow = "auto";
+});
